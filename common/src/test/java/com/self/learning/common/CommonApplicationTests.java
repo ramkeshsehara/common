4c6819b818ccc52ca90408415da935010b2308e2
ramkesh <ramkesh.meena@paytm.com>
2020-05-11 00:43:46 +0530

    added common file

---
+++ common/src/test/java/com/self/learning/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.learning.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
